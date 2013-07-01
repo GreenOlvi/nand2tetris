87779b39517698c8c2d94123b109715416a5417d
Piotr Szulc <szulo123@gmail.com>
2013-07-01 19:39:39 +0200

    Implemented Mult.asm

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+   @i
+   M=0
+   @R2
+   M=0
+
+(LOOP)
+   @R1
+   D=M
+   @i
+   D=D-M
+   @END
+   D;JEQ
+
+   @R0
+   D=M
+   @R2
+   M=D+M
+
+   @1
+   D=A
+   @i
+   M=D+M
+   @LOOP
+   0;JMP
+
+(END)
+   @END
+   0;JMP
